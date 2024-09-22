a2c88631e665d4c5abe82da710e18066f7499cb8
Gary Hale <gary@gradle.com>
2024-09-22 13:59:35 -0400

    Allow authors to disable extension registration for software types

--- platforms/core-configuration/declarative-dsl-provider/src/main/kotlin/org/gradle/internal/declarativedsl/project/ProjectInterpretationSequenceStep.kt
+++ platforms/core-configuration/declarative-dsl-provider/src/main/kotlin/org/gradle/internal/declarativedsl/project/ProjectInterpretationSequenceStep.kt
@@ -18,6 +18,7 @@ package org.gradle.internal.declarativedsl.project
 
 import org.gradle.internal.declarativedsl.evaluator.defaults.ApplyModelDefaults
 import org.gradle.internal.declarativedsl.evaluationSchema.SimpleInterpretationSequenceStepWithConversion
+import org.gradle.plugin.software.internal.SoftwareFeatureApplicator
 import org.gradle.plugin.software.internal.SoftwareTypeRegistry
 
 
@@ -26,9 +27,12 @@ import org.gradle.plugin.software.internal.SoftwareTypeRegistry
  * configured in the Settings DSL.
  */
 internal
-fun projectInterpretationSequenceStep(softwareTypeRegistry: SoftwareTypeRegistry) = SimpleInterpretationSequenceStepWithConversion(
+fun projectInterpretationSequenceStep(
+    softwareTypeRegistry: SoftwareTypeRegistry,
+    softwareFeatureApplicator: SoftwareFeatureApplicator
+) = SimpleInterpretationSequenceStepWithConversion(
     "project",
     features = setOf(ApplyModelDefaults()),
 ) {
-    projectEvaluationSchema(softwareTypeRegistry)
+    projectEvaluationSchema(softwareTypeRegistry, softwareFeatureApplicator)
 }
