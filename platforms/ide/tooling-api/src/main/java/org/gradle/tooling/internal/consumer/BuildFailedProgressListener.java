c50673a3462c637d0b5c31751e8345c789d038da
Donát Csikós <donat@gradle.com>
2024-09-24 18:17:56 +0200

    Add bw compat handling

--- platforms/ide/tooling-api/src/main/java/org/gradle/tooling/internal/consumer/BuildFailedProgressListener.java
+++ platforms/ide/tooling-api/src/main/java/org/gradle/tooling/internal/consumer/BuildFailedProgressListener.java
@@ -19,31 +19,52 @@ package org.gradle.tooling.internal.consumer;
 import org.gradle.api.NonNullApi;
 import org.gradle.tooling.BuildOutcomeHandler;
 import org.gradle.tooling.Failure;
+import org.gradle.tooling.events.FailureResult;
+import org.gradle.tooling.events.FinishEvent;
+import org.gradle.tooling.events.OperationResult;
 import org.gradle.tooling.events.ProgressEvent;
 import org.gradle.tooling.events.ProgressListener;
 import org.gradle.tooling.events.problems.BuildFailureEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @NonNullApi
 public class BuildFailedProgressListener implements ProgressListener {
     public List<Failure> failures;
-    private BuildOutcomeHandler buildOutcomeHandler;
+    private BuildOutcomeHandler buildOutcomeHandler = NOOP_HANDLER;
+    private boolean failureReported = false;
 
     @Override
     public void statusChanged(ProgressEvent event) {
         if (event instanceof BuildFailureEvent) {
-            BuildFailureEvent failureEvent = (BuildFailureEvent) event; // TODO (donat) BuildFailureWithProblemsEvent
+            BuildFailureEvent failureEvent = (BuildFailureEvent) event;
             this.failures = failureEvent.getFailures();
-            if (buildOutcomeHandler != null) {
-                buildOutcomeHandler.onFailure(failureEvent.getFailures());
+            buildOutcomeHandler.onFailure(failureEvent.getFailures());
+            failureReported = true;
+        }
+
+        if (event instanceof FinishEvent && event.getDescriptor().getParent() == null && !failureReported) {
+            OperationResult result = ((FinishEvent) event).getResult();
+            if (result instanceof FailureResult) {
+                buildOutcomeHandler.onFailure(new ArrayList<>(((FailureResult) result).getFailures()));
+            } else {
+                buildOutcomeHandler.onSuccess();
             }
-            // TODO (donat) onSuccess missing
-            // TODO (donat) handle onFailure BuildFailureEvent/BuildFailureWithProblemsEvent never arrives
         }
     }
 
     void setHandler(BuildOutcomeHandler buildOutcomeHandler) {
         this.buildOutcomeHandler = buildOutcomeHandler;
     }
+
+    private static BuildOutcomeHandler NOOP_HANDLER = new BuildOutcomeHandler() {
+        @Override
+        public void onSuccess() {
+        }
+
+        @Override
+        public void onFailure(List<Failure> failures) {
+        }
+    };
 }
